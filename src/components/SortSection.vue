95f3b7f718f8b858661744b9f51f3c209d97b95d
tseflcz <tseflcz@yeah.net>
2022-02-18 17:14:20 +0800

    Update SortSection.vue

--- src/components/SortSection.vue
+++ src/components/SortSection.vue
@@ -18,9 +18,7 @@ const sortBy = computed({
         <section-title title="排序" />
         <div class="section-content">
             <el-radio class="sort" v-model="sortBy" label="tot">按圣遗物评分</el-radio>
-            <el-radio class="sort" v-model="sortBy" label="md">按副词条评分</el-radio>
-            <el-radio class="sort" v-model="sortBy" label="avg">按期望词条数</el-radio>
-            <el-radio class="sort" v-model="sortBy" label="max">按最大词条数</el-radio>
+            <el-radio class="sort" v-model="sortBy" label="md">按期望词条数</el-radio>
             <el-radio class="sort" v-model="sortBy" label="cur">按当前词条数</el-radio>
             <el-radio class="sort" v-model="sortBy" label>不排序</el-radio>
         </div>
